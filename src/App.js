22be19c305816f7793e00808d047d1e5714cd876
awasthi <you@example.com>
2021-09-16 22:37:46 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,176 @@
+import "./App.css";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect, useState } from "react";
+import Comment from "./components/Comment";
+import Loader from "./components/Loader";
+import EndMsg from "./components/EndMsg";
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+  const [page, setpage] = useState(2);
+
+  useEffect(() => {
+    const getComments = async () => {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20`
+        // For json server use url below
+        // `http://localhost:3004/comments?_page=1&_limit=20`
+      );
+      const data = await res.json();
+      setItems(data);
+    };
+
+    getComments();
+  }, []);
+
+  const fetchComments = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=20`
+      // For json server use url below
+      // `http://localhost:3004/comments?_page=${page}&_limit=20`
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const fetchData = async () => {
+    const commentsFormServer = await fetchComments();
+
+    setItems([...items, ...commentsFormServer]);
+    if (commentsFormServer.length === 0 || commentsFormServer.length < 20) {
+      sethasMore(false);
+    }
+    setpage(page + 1);
+  };
+
+ 
+// console.log(page,'page')
+
+  return (
+    <InfiniteScroll
+      dataLength={items.length} //This is important field to render the next data
+      next={fetchData}
+      hasMore={hasMore}
+      loader={<Loader />}
+      endMessage={<EndMsg />}
+    >
+      <div className="container">
+        <div className="row m-2">
+          {items.map((item) => {
+            return <Comment key={item.id} item={item} />;
+          })}
+        </div>
+      </div>
+    </InfiniteScroll>
+  );
+}
+
+export default App;
+
+
+
+
+//refer from here video      
+
+//https://www.youtube.com/watch?v=bBUOMy6Tugw
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import Comment from "./components/Comment";
+
+
+
+
+// function App() {
+
+//   const [items, setItems] = useState([]);
+//   const [hasMore, sethasMore] = useState(true);
+//   const [page, setpage] = useState(2);
+
+//   useEffect(()=>{
+//     const getComments = async () => {
+//       const res = await fetch(
+//         `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20`
+//         // For json server use url below
+//         // `http://localhost:3004/comments?_page=1&_limit=20`
+//       );
+//       const data = await res.json();
+//       setItems(data);
+//     };
+
+//     getComments();
+//   })
+
+
+//   // console.log(items)
+
+//   const fetchComments = async () => {
+//     const res = await fetch(
+//       `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=20`
+//       // For json server use url below
+//       // `http://localhost:3004/comments?_page=${page}&_limit=20`
+//     );
+//     const data = await res.json();
+//     return data;
+//   };
+
+
+
+//   const fetchData = async () => {
+//     const commentsFormServer = await fetchComments();
+//     setItems([...items, ...commentsFormServer]);
+//     if (commentsFormServer.length === 0 || commentsFormServer.length < 20) {
+//       sethasMore(false);
+//     }
+//     setpage(page + 1);
+//   };
+
+
+//   return (
+//     <>
+//           <InfiniteScroll
+//                 dataLength={items.length} //This is important field to render the next data
+//                 next={fetchData}
+//                 hasMore={hasMore}
+//                 loader={<h4>Loading...</h4>}
+//                 endMessage={
+//                   <p style={{ textAlign: 'center' }}>
+//                     <b>Yay! You have seen it all</b>
+//                   </p>
+//                 }
+  
+//             >
+//                     {items.map((item) => {
+//                              return <Comment key={item.id} item={item} />;
+//                       })}
+//         </InfiniteScroll>
+//     </>
+//   )
+// }
+
+// export default App
+
